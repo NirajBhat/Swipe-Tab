fa7bb32c78e5821c2c7988b5566713d06dd32167
chhota89 <chhota89@gmail.com>
2016-11-08 12:00:39 +0530

    View Pager/Swipe tab

---
+++ app/src/main/java/com/example/bridgeit/swipetab/Tab2.java
@@ -0,0 +1,19 @@
+package com.example.bridgeit.swipetab;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bridgeit on 7/11/16.
+ */
+public class Tab2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab2,container,false);
+    }
+}
